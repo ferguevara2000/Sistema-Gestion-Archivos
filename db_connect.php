f8f887f098cc7b8ba7d8deea18694f6792af3bcc
Kevin-Xtrem <kevinxtrem2022@gmail.com>
2024-01-17 19:02:30 -0500

    Cambio de la conexion patron singleton

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,48 @@
-<?php 
+<?php
 
-$conn= new mysqli('localhost','root','','filesystem')or die("No se pudo conectar a MySQL".mysqli_error($con));
+class Database {
+    private static $instance;
+    private $conn;
+    private $servername = 'localhost';
+    private $username = 'root';
+    private $password = '';
+    private $database = 'filesystem';
+
+    private function __construct() {
+        // Crear conexión
+        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->database);
+
+        // Verificar conexión
+        if ($this->conn->connect_error) {
+            die("No se pudo conectar a MySQL: " . $this->conn->connect_error);
+        }
+    }
+
+    public static function getInstance() {
+        if (!self::$instance) {
+            self::$instance = new Database();
+        }
+
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+
+    public function closeConnection() {
+        if ($this->conn) {
+            $this->conn->close();
+        }
+    }
+}
+
+/* // Obtener la instancia de la conexión
+$db = Database::getInstance();
+$conn = $db->getConnection();
+
+// Resto del código aquí...
+
+// Cerrar la conexión al finalizar
+$db->closeConnection(); */
+?>
